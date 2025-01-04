9b915865b22f11e85117b7c2e7f1ed8dc9bbbc68
Nikita Kovalenko <nik.koval.89@gmail.com>
2025-01-04 20:05:19 -0300

    feature(link): add entity, repo and json handler

---
+++ src/main/java/org/abigotado/repository/impl/LinkRepositoryImpl.java
@@ -0,0 +1,51 @@
+package org.abigotado.repository.impl;
+
+import org.abigotado.entity.Link;
+import org.abigotado.repository.LinkRepository;
+import org.abigotado.utils.JsonFileHandler;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+public class LinkRepositoryImpl implements LinkRepository {
+    private final Map<String, Link> linkStorage;
+
+    public LinkRepositoryImpl() {
+        this.linkStorage = JsonFileHandler.loadLinks();
+    }
+
+    @Override
+    public Optional<Link> findByShortLink(String shortLink) {
+        return Optional.ofNullable(linkStorage.get(shortLink));
+    }
+
+    @Override
+    public void saveLink(Link link) {
+        Optional<Link> savedLink = Optional.ofNullable(linkStorage.put(link.getShortLink(), link));
+        JsonFileHandler.saveLinks(linkStorage);
+    }
+
+    @Override
+    public void deleteExpiredLinks() {
+        linkStorage.entrySet().removeIf(entry -> entry.getValue().getExpirationDate().isBefore(java.time.LocalDateTime.now()));
+        JsonFileHandler.saveLinks(linkStorage);
+    }
+
+    @Override
+    public void decrementClicksLeft(UUID linkId) {
+        linkStorage.values().stream()
+                   .filter(link -> link.getId().equals(linkId))
+                   .findFirst()
+                   .ifPresent(link -> {
+                       link.setClicksLeft(link.getClicksLeft() - 1);
+                       JsonFileHandler.saveLinks(linkStorage);
+                   });
+    }
+
+    @Override
+    public Collection<Link> findAll() {
+        return linkStorage.values();
+    }
+}
