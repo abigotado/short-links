51cc866b3f23e244fa3310d71d555a949b5901b0
Nikita Kovalenko <nik.koval.89@gmail.com>
2025-01-06 20:07:23 -0300

    refactor(link): minor refactoring

--- src/main/java/org/abigotado/links/repository/LinkRepository.java
+++ src/main/java/org/abigotado/links/repository/LinkRepository.java
@@ -7,11 +7,16 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface LinkRepository {
-     void saveLink(Link link);
-     Optional<Link> findByShortLink(String shortLink);
-     Optional<Link> findByLongLinkAndUserId(String longLink, UUID userId);
-     void deleteExpiredLinks();
-     void decrementClicksLeft(UUID linkId);
+    void saveLink(Link link);
+
+    Optional<Link> findByShortLink(String shortLink);
+
+    Optional<Link> findByLongLinkAndUserId(String longLink, UUID userId);
+
+    void deleteExpiredLinks();
+
+    void decrementClicksLeft(UUID linkId);
+
     Collection<Link> findAll();
 
 }
